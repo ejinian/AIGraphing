6e3d9cb6b95f1bdf54cfcca74f56e2bf8b88166b
Ernest <ernest.jinian@gmail.com>
2020-04-15 12:20:17 -0700

    initial commit

---
+++ Graphing/Graphing/Program.cs
@@ -0,0 +1,16 @@
+﻿using AwokeKnowing.GnuplotCSharp;
+using System;
+
+
+namespace Graphing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GnuPlot.Plot("sin(x) + 2");
+            GnuPlot.Write("move");
+            Console.ReadLine();
+        }
+    }
+}
